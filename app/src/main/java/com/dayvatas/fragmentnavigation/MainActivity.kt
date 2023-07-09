b774f93c95e3effb82d4fd42cec45e4a43a2a287
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-07-09 14:43:31 +0300

    appcompat bugs solved

--- app/src/main/java/com/dayvatas/fragmentnavigation/MainActivity.kt
+++ app/src/main/java/com/dayvatas/fragmentnavigation/MainActivity.kt
@@ -1,46 +1,21 @@
 package com.dayvatas.fragmentnavigation
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.dayvatas.fragmentnavigation.ui.theme.FragmentNavigationTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FragmentNavigationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
-}
+    fun button1clicked(view : View){
+        val fragmentManager = supportFragmentManager
+    }
+    fun button2clicked(view : View){
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FragmentNavigationTheme {
-        Greeting("Android")
     }
 }
