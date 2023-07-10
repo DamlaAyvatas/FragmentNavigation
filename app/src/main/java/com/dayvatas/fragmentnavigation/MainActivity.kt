00ebef9a29abe1b8619bff0a228a7afeb56a3d3d
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-07-10 23:28:36 +0300

    navigation def to gradle

--- app/src/main/java/com/dayvatas/fragmentnavigation/MainActivity.kt
+++ app/src/main/java/com/dayvatas/fragmentnavigation/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.Navigation
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
     fun button1clicked(view : View){
         val fragmentManager = supportFragmentManager
